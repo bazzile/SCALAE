38e373ebe8654f88b0b8827dfc1b12ddad2db6c6
Vasily Lobanov <lobanov.vk@gmail.com>
2024-05-25 10:43:00 +0300

    test

--- setup_vm.sh
+++ setup_vm.sh
@@ -5,14 +5,15 @@ sudo apt update
 # sudo apt upgrade -y
 
 # Install Python 3.8 and necessary Python tools
-sudo apt install -y python3.8 python3.8-venv python3.8-dev python3-pip git s3fs
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install -y python3.7 python3.7-venv python3.7-dev python3-pip git s3fs screen
 
 # Add Python 3.8 to the alternatives system and set it as default
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
-sudo update-alternatives --set python3 /usr/bin/python3.8
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
+sudo update-alternatives --set python3 /usr/bin/python3.7
 
 # Create a symbolic link for python to point to python3 and pip3
-sudo ln -s /usr/bin/python3.8 /usr/bin/python
+sudo ln -s /usr/bin/python3.7 /usr/bin/python
 sudo ln -s /usr/bin/pip3 /usr/bin/pip
 # Verify pip installation
 pip3 --version
